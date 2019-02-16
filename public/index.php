922128bfbd6c73b1a7f444c5619bdeb1ec3c577a
ajm <ajm257@njit.edu>
2019-02-16 18:00:18 -0500

    Feature: Started calling process to pass in file through the getRecords function to return data from file as an array of arrays

--- public/index.php
+++ public/index.php
@@ -9,27 +9,39 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-main::start();
+main::start("example.csv");
 
 class main {
 
-    static public function start () {
-        $records = csv::getRecords();
-        $table = html::generateTable($records);
-        system::printPage($table);
+    static public function start ($filename) {
+
+        $records = csv::getRecords($filename);
+
+        print_r($records);
     }
 }
 
 class csv {
 
-    static public function getRecords() {
+    static public function getRecords($filename) {
 
-        $records = 'test sjhsdf';
+        $file = fopen($filename,"r");
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            $records[] = $record;
+        }
+
+        fclose($file);
+
         return $records;
     }
 }
 
-class html {
+
+/* class html {
 
     static public function generateTable($records) {
 
@@ -45,5 +57,5 @@ class system {
 
         echo $page;
     }
-}
+} */
 
