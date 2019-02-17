0aff349facf3ae58e49073b01c28fdf21ff7ee50
ajm <ajm257@njit.edu>
2019-02-16 20:00:54 -0500

    Feature: Loop through the record array to put the fieldNames and values in position in the record object through the createProperty function

--- public/index.php
+++ public/index.php
@@ -60,9 +60,14 @@ class record { //object instantiation
     public function __construct(Array $fieldNames = null, $values = null) {
 
         $record = array_combine($fieldNames, $values);
-        print_r($record);
 
-        $this->createProperty();
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+
+        }
+        
+        print_r($this);
+
 
     }
 
