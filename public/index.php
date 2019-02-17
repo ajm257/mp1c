2a6a7d9b0e1ce20b51489e4d5f5ae376cb4bb79d
ajm <ajm257@njit.edu>
2019-02-16 22:37:23 -0500

    Fix: generateTable function loops through array to first get the keys to make table headings,then in the second iteration print the table rows of the additional values. Places array in the order in which the table will print

--- public/index.php
+++ public/index.php
@@ -102,9 +102,29 @@ class html
     static public function generateTable($records)
     {
 
+        $count = 0;
+
         foreach ($records as $record) {
-            $array = $record->returnArray();
-            print_r($array);
+
+            if($count ==0) {
+
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+
+
+            }else {
+
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+
+            }
+
+            $count++;
+
 
             /*$table = $records;
 
