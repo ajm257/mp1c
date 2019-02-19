00c573e34ce7dc7f0db29f446c527985440a8f08
ajm <ajm257@njit.edu>
2019-02-19 16:00:09 -0500

    Feature: Add title and body tags

--- public/index.php
+++ public/index.php
@@ -135,10 +135,10 @@ class html
 
         $html = '<html>';
 
-        $html .= '<head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        $html .= '<head><title>My Mini Project IS601</title><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script></head>';
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script></head><body>';
 
         $html .= '<table class="table table-striped">'; //start table
 
@@ -146,7 +146,7 @@ class html
 
         $html .= self::returnRow($records);
 
-        $html .= '</table> </html>';
+        $html .= '</table></body> </html>';
         return $html;
     }
 
