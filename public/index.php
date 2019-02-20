712a5b14212cf11352adb5caaaa0bd66e565a611
ajm <ajm257@njit.edu>
2019-02-19 20:44:29 -0500

    Feature: Add html for formatting table to its own function then call back in return table function

--- public/index.php
+++ public/index.php
@@ -91,9 +91,16 @@ class html
     static public function returnTable($table) {
 
 
-        $html = "<table> $table </table>";
+        $htmlTable = '';
 
-        return $html;
+        $htmlTable .= '<head><title>My Mini Project IS601</title><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script></head><body>';
+
+        $htmlTable .= '<table class="table table-striped">';
+
+        return $htmlTable;
 
 
     }
@@ -133,14 +140,8 @@ class html
     static public function generateTable($records)
     {
 
-        $html = '<html>';
 
-        $html .= '<head><title>My Mini Project IS601</title><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script></head><body>';
-
-        $html .= '<table class="table table-striped">'; //start table
+        $html = self::returnTable($records);
 
         $html .= self::returnHeader($records[0]);
 
